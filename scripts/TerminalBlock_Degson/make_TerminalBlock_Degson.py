afd84cb5a86a463bb4c38f340d6e9112565c75b0
Søren Thing Andersen <soeren@thing.dk>
2018-06-23 13:32:53 +0200

    Degson 2CDGRM-5.08-XXP-1Y-00A(H) - 2-22 pins

--- scripts/TerminalBlock_Degson/make_TerminalBlock_Degson.py
+++ scripts/TerminalBlock_Degson/make_TerminalBlock_Degson.py
@@ -86,7 +86,110 @@ def make_DG242V_7_5():
                                   tags_additional=[], lib_name='${KISYS3DMOD}/'+classname, classname=classname, classname_description=classname_description,
                                   webpage=webpage, script_generated_note=script_generated_note)
 
+
+def make_2CDGRM_5_08():
+    """2CDGRM-5.08 - http://www.degson.com/en/pro_detail/id/689"""
+    pins=range(2, 22+1)
+    for p in pins:
+        footprint_name = "2CDGRM-5.08-{0:02d}P-1Y-00A_1x{0:02d}_P5.08_Horizontal".format(p)
+        print(footprint_name)
+
+        dx = 5.08
+        y_mounting = 1.9
+        sx = (p - 1) * dx + 15.24  # X size without the flange
+        sx_flange = sx + 2 * 1.4
+        sy = 12
+        drill_pins = 1.7
+        drill_mounting = 2
+        pad = 3
+
+        # init kicad footprint
+        kicad_mod = Footprint(footprint_name)
+        kicad_mod.setDescription(
+            "Terminal Block Degson 2CDGRM-5.08-{0:02d}P-1Y-00A(H) {0} pins, flanged,"
+            " pitch 5.08mm, size {1}x{2}mm^2, drill diameter {3}mm, pad diameter {4}mm,"
+            " see http://www.degson.com/en/pro_detail/id/689, script-generated using"
+            " https://github.com/pointhi/kicad-footprint-generator/tree/master/scripts/TerminalBlock_Degson"
+            .format(p, sx_flange, sy, drill_pins, pad)
+            )
+        kicad_mod.setTags(
+            "THT Terminal Block Degson 2CDGRM-5.08-{:02d}P-1Y-00A(H) flanged pitch 5.08mm"
+            .format(p))
+
+        # create pads
+        x = 0
+        for p in range(1,p+1):
+            if p == 1:
+                shape = Pad.SHAPE_RECT
+            else:
+                shape = Pad.SHAPE_CIRCLE
+            kicad_mod.append(Pad(number=p, type=Pad.TYPE_THT, shape=shape,
+                                 at=[x, 0], size=pad, drill=drill_pins, layers=Pad.LAYERS_THT))
+            x = x + dx
+
+        # create mounting holes
+        for p in [-1, p]:
+            x = p * dx
+            kicad_mod.append(Pad(type=Pad.TYPE_NPTH, shape=Pad.SHAPE_CIRCLE,
+                                 at=[x, y_mounting], size=drill_mounting, drill=drill_mounting, layers=Pad.LAYERS_THT))
+
+        #
+        # Silkscreen
+        #
+        xc = (p-1)/2*dx
+        kicad_mod.append(Text(type='reference', text='REF**', at=[xc, -3], layer='F.SilkS'))
+        # Inside half
+        x1 = -1.5 * dx
+        x2 = (p-1)*dx-x1
+        kicad_mod.append(RectLine(start=[x1, -2.1], end=[x2, 3.8], layer='F.SilkS'))
+        # Pin-1 designator
+        kicad_mod.append(Line(start=[x1-0.5, -2.6], end=[x1+2.5, -2.6], layer='F.SilkS'))
+        kicad_mod.append(Line(start=[x1-0.5, -2.6], end=[x1-0.5, 0.4], layer='F.SilkS'))
+        # Flange
+        xf1 = -9.02
+        xf2 = (p-1)*dx-xf1
+        kicad_mod.append(RectLine(start=[xf1, 3.8], end=[xf2, 4.6], layer='F.SilkS'))
+        # Foam
+        kicad_mod.append(FilledRect(start=[xf1, 4.6], end=[xf2, 5.6], layer='F.SilkS'))
+        # Outside half
+        kicad_mod.append(RectLine(start=[x1, 5.6], end=[x2, 9.9], layer='F.SilkS'))
+        kicad_mod.append(Line(start=[x1, 7], end=[x2, 7], layer='F.SilkS'))
+        kicad_mod.append(Line(start=[x1, 8.7], end=[x2, 8.7], layer='F.SilkS'))
+        for p in range(-1, p+1):
+            x = p * dx
+            kicad_mod.append(Line(start=[x-0.5, 8.7], end=[x-0.8, 9.9], layer='F.SilkS'))
+            kicad_mod.append(Line(start=[x+0.5, 8.7], end=[x+0.8, 9.9], layer='F.SilkS'))
+
+        #
+        # Fabrication
+        #
+        yfab = 7.9
+        if p < 6:
+            yfab = 10.8
+        kicad_mod.append(Text(type='value', text=footprint_name, at=[xc, yfab], layer='F.Fab'))
+        kicad_mod.append(Text(type='user', text='%R', at=[xc, 4], layer='F.Fab'))
+        # Simplified outline
+        kicad_mod.append(RectLine(start=[x1, -2.1], end=[x2, 9.9], layer='F.Fab'))
+        # Pin-1 designator
+        kicad_mod.append(Line(start=[-1.9, -1.9], end=[1.1, -1.9], layer='F.Fab'))
+        kicad_mod.append(Line(start=[-1.9, -1.9], end=[-1.9, 1.1], layer='F.Fab'))
+
+        #
+        # Courtyard
+        #
+        kicad_mod.append(RectLine(start=[xf1-0.5, -2.6], end=[xf2+.5, 30], layer='F.CrtYd'))
+
+        # add model
+        kicad_mod.append(Model(
+            filename="${KISYS3DMOD}" + "/TerminalBlock_Degson.3dshapes/{}.wrl".format(footprint_name),
+            at=[0, 0, 0], scale=[1, 1, 1], rotate=[0, 0, 0]))
+
+        # output kicad model
+        file_handler = KicadFileHandler(kicad_mod)
+        file_handler.writeFile('{}.kicad_mod'.format(footprint_name))
+
+
 if __name__ == '__main__':
     make_DG212V_THR_3_5()
     make_DG242V_7_5()
-   
+    make_2CDGRM_5_08()
