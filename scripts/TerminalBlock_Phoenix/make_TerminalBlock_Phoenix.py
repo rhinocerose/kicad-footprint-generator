361d8fd9c3bef9a43cb455231abb68bf6de3c0fd
Henning Kleen <kleen@cosynth.com>
2020-01-08 10:26:17 +0100

    Add generator for Phoenix Contact MKDS 1 / X-3,5 terminal blocks

--- scripts/TerminalBlock_Phoenix/make_TerminalBlock_Phoenix.py
+++ scripts/TerminalBlock_Phoenix/make_TerminalBlock_Phoenix.py
@@ -100,6 +100,39 @@ if __name__ == '__main__':
    
  
     pins=range(2,16+1)
+    rm=3.5
+    package_height=7.3
+    leftbottom_offset=[2, 3.6]
+    ddrill=1.1
+    pad=[2.2,2.2]
+    screw_diameter=3
+    bevel_height=[0.6,1.8,5.5]
+    slit_screw=True
+    screw_pin_offset=[0,0]
+    secondHoleDiameter=0
+    secondHoleOffset=[0,0]
+    thirdHoleDiameter=0
+    thirdHoleOffset=[0,-4]
+    fourthHoleDiameter=0
+    fourthHoleOffset=[0,0]
+    fabref_offset=[0,2.5]
+    nibbleSize=[]
+    nibblePos=[]
+    for p in pins:
+        name="MKDS-1-{0}-{1:2.3}".format(p,rm);
+        webpage="https://media.digikey.com/PDF/Data%20Sheets/Phoenix%20Contact%20PDFs/1751248.pdf";
+        classname_description="Terminal Block Phoenix {0}".format(name);
+        footprint_name="TerminalBlock_Phoenix_{0}_1x{2:02}_P{1:3.2f}mm_Horizontal".format(name, rm, p)
+        makeTerminalBlockStd(footprint_name=footprint_name,
+                                  pins=p, rm=rm,
+                                  package_height=package_height, leftbottom_offset=leftbottom_offset,
+                                  ddrill=ddrill, pad=pad, screw_diameter=screw_diameter, bevel_height=bevel_height, slit_screw=slit_screw, screw_pin_offset=screw_pin_offset, secondHoleDiameter=secondHoleDiameter, secondHoleOffset=secondHoleOffset, thirdHoleDiameter=thirdHoleDiameter, thirdHoleOffset=thirdHoleOffset, fourthHoleDiameter=fourthHoleDiameter, fourthHoleOffset=fourthHoleOffset, 
+                                  nibbleSize=nibbleSize, nibblePos=nibblePos, fabref_offset=fabref_offset,
+                                  tags_additional=[], lib_name='${KISYS3DMOD}/'+classname, classname=classname, classname_description=classname_description,
+                                  webpage=webpage, script_generated_note=script_generated_note)
+
+
+    pins=range(2,16+1)
     rm=5.08
     package_height=9.8
     leftbottom_offset=[rm/2, 4.6]
