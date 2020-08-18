33fed24595f2225467fdbe28bf994b08ec7af1d7
Carlos Nieves <cnieves.mail@gmail.com>
2020-08-18 11:56:33 +0200

    Added Phoenix SPT-1.5-xx-H-3.5 horizontal terminal block series. Datasheet: https://media.digikey.com/pdf/Data%20Sheets/Phoenix%20Contact%20PDFs/1990740.pdf

--- scripts/TerminalBlock_Phoenix/make_TerminalBlock_Phoenix.py
+++ scripts/TerminalBlock_Phoenix/make_TerminalBlock_Phoenix.py
@@ -100,6 +100,43 @@ if __name__ == '__main__':
    
  
     pins=range(2,16+1)
+    rm=3.5
+    package_height=14.4
+    leftbottom_offset=[1.3, 3.67, 3.6]
+    ddrill=1.1
+    pad=[1.8,3]
+    screw_diameter=0
+    bevel_height=[]
+    slit_screw=False
+    screw_pin_offset=[0,0]
+    secondDrillDiameter=ddrill
+    secondDrillOffset=[0,-8.2]
+    secondDrillPad=pad
+    secondHoleDiameter=[1,1]
+    secondHoleOffset=[0,6.5]
+    thirdHoleDiameter=0
+    thirdHoleOffset=[0,-4]
+    fourthHoleDiameter=0
+    fourthHoleOffset=[0,0]
+    fabref_offset=[0,0]
+    nibbleSize=[]
+    nibblePos=[]
+    for p in pins:
+        name="SPT-1,5-{0}-H-{1:1.2}".format(p,rm);
+        webpage="https://media.digikey.com/pdf/Data%20Sheets/Phoenix%20Contact%20PDFs/1990740.pdf";
+        classname_description="Terminal Block Phoenix {0}".format(name);
+        footprint_name="TerminalBlock_Phoenix_{0}_1x{2:02}_P{1:3.2f}mm_Horizontal".format(name, rm, p)
+        makeTerminalBlockStd(footprint_name=footprint_name, 
+                                  pins=p, rm=rm, 
+                                  package_height=package_height, leftbottom_offset=leftbottom_offset, 
+                                  ddrill=ddrill, pad=pad, screw_diameter=screw_diameter, bevel_height=bevel_height, slit_screw=slit_screw, screw_pin_offset=screw_pin_offset, secondHoleDiameter=secondHoleDiameter, secondHoleOffset=secondHoleOffset, thirdHoleDiameter=thirdHoleDiameter, thirdHoleOffset=thirdHoleOffset, fourthHoleDiameter=fourthHoleDiameter, fourthHoleOffset=fourthHoleOffset, 
+                                  secondDrillDiameter=secondDrillDiameter,secondDrillOffset=secondDrillOffset,secondDrillPad=secondDrillPad,
+                                  nibbleSize=nibbleSize, nibblePos=nibblePos, fabref_offset=fabref_offset,
+                                  tags_additional=[], lib_name='${KISYS3DMOD}/'+classname, classname=classname, classname_description=classname_description, 
+                                  webpage=webpage, script_generated_note=script_generated_note)
+   
+ 
+    pins=range(2,16+1)
     rm=5.08
     package_height=9.8
     leftbottom_offset=[rm/2, 4.6]
